17a81ed262dbe9bef0470d9faa9bfd7d68d69a0e
yumancang <mancangluo@tencent.com>
2020-03-19 19:25:43 +0800

    权限问题

--- docker/bin/web-queue-server.sh
+++ docker/bin/web-queue-server.sh
@@ -4,16 +4,13 @@ echo "run web-queue-server.sh"
 
 source /etc/profile
 
-nscd
-
 cd /home/user00/web
-sudo -u user00 cp .env.example .env
-sudo -u user00 /usr/local/bin/composer install
-sudo -u user00 /usr/local/php/bin/php artisan key:generate
-sudo -u user00 /usr/local/php/bin/php artisan jwt:secret
-sudo -u user00 /usr/local/php/bin/php artisan migrate
-sudo -u user00 /usr/local/php/bin/php artisan db:seed
-sudo -u user00 /usr/local/php/bin/php artisan optimize
+cp /home/user00/web/.env.example /home/user00/web/.env
+/usr/local/bin/composer install
+/usr/local/php/bin/php artisan key:generate
+/usr/local/php/bin/php artisan jwt:secret
+/usr/local/php/bin/php artisan migrate
+/usr/local/php/bin/php artisan db:seed
 
 
 
