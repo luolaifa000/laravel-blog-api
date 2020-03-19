c167301997f4412aaf9c212a33fb0bdb146605cf
yumancang <mancangluo@tencent.com>
2020-03-19 19:35:18 +0800

    111

--- docker/bin/web-queue-server.sh
+++ docker/bin/web-queue-server.sh
@@ -7,13 +7,10 @@ source /etc/profile
 cd /home/user00/web
 cp /home/user00/web/.env.example /home/user00/web/.env
 /usr/local/bin/composer install
-/usr/local/php/bin/php artisan key:generate
-/usr/local/php/bin/php artisan jwt:secret
+#/usr/local/php/bin/php artisan key:generate
+#/usr/local/php/bin/php artisan jwt:secret
 /usr/local/php/bin/php artisan config:clear
-/usr/local/php/bin/php artisan migrate
-/usr/local/php/bin/php artisan db:seed
-
-
+/usr/local/php/bin/php artisan migrate --seed
 
 php-fpm
 nginx -g "daemon off;"
